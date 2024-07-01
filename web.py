687e2df523f4bff3a0e224e542190bb31c947e15
Ver <veronicawyg@gmail.com>
2024-07-01 14:52:04 +0200

    webapp updated (modified layout and added a new page)

--- web.py
+++ web.py
@@ -3,7 +3,7 @@ from modules import functions
 
 todos = functions.get_todos()
 
-
+st.set_page_config(layout="wide")
 def add_todo():
     new_todo = st.session_state["new_todo"] + "\n"
     todos.append(new_todo)
@@ -13,7 +13,11 @@ def add_todo():
 todos = functions.get_todos()
 st.title("My Todo App")
 st.subheader("This is my todo app")
-st.write("This app is to increase your productivity")
+st.write("This app is to increase your <b>productivity</b>",
+         unsafe_allow_html=True)
+
+st.text_input(label="", placeholder="Add a new todo...",
+              on_change=add_todo, key="new_todo")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -23,7 +27,6 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun()
 
-st.text_input(label="Enter a todo", placeholder="Add a new todo...",
-              on_change=add_todo, key="new_todo")
+
 
 
